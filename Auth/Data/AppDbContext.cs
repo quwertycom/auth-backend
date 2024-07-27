56f8df0da97692e51a0436ac79082a1dfe570e64
hagrid862 <n.okroj@icloud.com>
2024-07-27 13:48:44 +0200

    feat: created empty app db context

---
+++ Auth/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Auth.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    //TODO: Add Models
+}
