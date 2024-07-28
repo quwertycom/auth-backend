bd8e48a1a3884d72cacbf2344d47e3ea0a4a5312
hagrid862 <n.okroj@icloud.com>
2024-07-28 21:30:07 +0200

    feat: added data validation and executing of service method in user controller on register route

--- Auth/Controllers/UserControllers.cs
+++ Auth/Controllers/UserControllers.cs
@@ -28,9 +28,50 @@ public class UserController : ControllerBase
     [HttpPost("auth/register")]
     public async Task<ActionResult<UserRegisterResponseModel>> Register([FromBody] UserRegisterRequestModel request)
     {
-        return Ok(new UserRegisterResponseModel
+        var validators = new Dictionary<Func<UserRegisterRequestModel, bool>, string>
         {
-            Status = "Success",
-        });
+            { r => string.IsNullOrEmpty(r.Email), "EMPTY_EMAIL" },
+            { r => !r.Email.Contains("@") || !r.Email.Contains("."), "INVALID_EMAIL" },
+            { r => string.IsNullOrEmpty(r.FirstName), "EMPTY_FIRST_NAME" },
+            { r => r.FirstName.Length < 3 || r.FirstName.Length > 32, "INVALID_FIRST_NAME" },
+            { r => string.IsNullOrEmpty(r.LastName), "EMPTY_LAST_NAME" },
+            { r => r.LastName.Length < 3 || r.LastName.Length > 32, "INVALID_LAST_NAME" },
+            { r => string.IsNullOrEmpty(r.Password), "EMPTY_PASSWORD" },
+            { r => r.Password.Length < 8 || r.Password.Length > 32 || r.Password.Any(char.IsDigit) || r.Password.Any(char.IsLetter), "INVALID_PASSWORD" },
+            { r => r.Phone != null && (r.Phone.Length < 5 || r.Phone.Length > 15 || r.Phone.All(char.IsDigit)), "INVALID_PHONE" },
+            { r => string.IsNullOrEmpty(r.Username), "EMPTY_USERNAME" },
+            { r => r.Username.Length < 3 || r.Username.Length > 32 || r.Username.Any(char.IsWhiteSpace), "INVALID_USERNAME" },
+            { r => string.IsNullOrEmpty(r.BirthDate), "EMPTY_BIRTH_DATE" },
+            { r => !DateTime.TryParse(r.BirthDate, out var birthDate) || birthDate > DateTime.UtcNow.AddYears(-17), "INVALID_BIRTH_DATE" },
+            { r => string.IsNullOrEmpty(r.Gender), "EMPTY_GENDER" },
+        };
+
+        foreach (var validator in validators)
+        {
+            if (validator.Key(request))
+            {
+                return BadRequest(new UserRegisterResponseModel
+                {
+                    Status = validator.Value
+                });
+            }
+        }
+
+        var result = await _userService.Register(request);
+
+        if (result.isSuccess)
+        {
+            return Ok(new UserRegisterResponseModel
+            {
+                Status = "Success"
+            });
+        }
+        else
+        {
+            return BadRequest(new UserRegisterResponseModel
+            {
+                Status = result.status
+            });
+        }
     }
 }
