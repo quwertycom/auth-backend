83408d500cc47d4fe5a11a408c40fae5af4c4234
hagrid862 <n.okroj@icloud.com>
2024-07-28 21:53:25 +0200

    feat: added register user method into user service

--- Auth/Services/UserService.cs
+++ Auth/Services/UserService.cs
@@ -1,4 +1,6 @@
 using Auth.Data;
+using Auth.Helpers;
+using Auth.Models;
 using Auth.Models.RequestModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -29,6 +31,45 @@ public class UserService : IUserService
     {
         try
         {
+            var passwordSalt = Password.GenerateSalt();
+            var passwordHash = Password.HashPassword(model.Password, passwordSalt);
+
+            var @user = new User
+            {
+                Id = Snowflake.Next(),
+                Username = model.Username,
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Email = model.Email,
+                Avatar = "name",
+                BirthDate = model.BirthDate,
+                Gender = model.Gender,
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt,
+                Accounts = new List<Account>(),
+                Sessions = new List<UserSession>(),
+                Devices = new List<Device>(),
+                CreatedAt = DateTime.UtcNow,
+            };
+
+            var @personalAccount = new Account
+            {
+                Id = Snowflake.Next(),
+                Name = "Personal",
+                Avatar = "name",
+                User = @user,
+                Sessions = new List<AccountSession>(),
+                Applications = new List<ApplicationAccount>(),
+                Developers = new List<Developer>(),
+                CreatedAt = DateTime.UtcNow,
+                UserId = @user.Id
+            };
+
+            @user.Accounts.Add(@personalAccount);
+
+            _context.Users.Add(@user);
+            await _context.SaveChangesAsync();
+
             return (true, "OK");
         }
         catch
