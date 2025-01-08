ddef8ac6eaf9a93de73e2e1d0249477f80e4b4de
Nikodem Okroj <n.okroj@icloud.com>
2025-01-08 09:18:50 +0100

    feat: add Token and UserSession models for token management and user session tracking
    - Introduced the Token model to represent authentication tokens, including properties for TokenString, Type, User, UserId, CreatedAt, and optional ExpiresAt.
    - Added the UserSession model to manage user sessions, featuring properties for User, UserId, a collection of Tokens, CreatedAt, and LastUsedAt.
    - These additions enhance the API's capabilities for managing user authentication and session states, aligning with previous efforts to improve model organization and functionality.

---
+++ API/Models/Token.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using API.Common.Enums;
+namespace API.Models;
+
+public class Token {
+    [Key]
+    public long Id { get; set; }
+
+    [Required]
+    public required string TokenString { get; set; }
+
+    [Required]
+    public required TokenType Type { get; set; }
+
+    [Required]
+    public required User User { get; set; }
+
+    [Required]
+    public required long UserId { get; set; }
+
+    [Required]
+    public required DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public DateTime? ExpiresAt { get; set; } = null;
+}
---
+++ API/Models/UserSession.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models;
+
+public class UserSession {
+    [Key]
+    public long Id { get; set; }
+
+    [Required]
+    public required User User { get; set; }
+
+    [Required]
+    public long UserId { get; set; }
+
+    [Required]
+    public virtual ICollection<Token> Tokens { get; set; } = new List<Token>();
+
+    [Required]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public DateTime? LastUsedAt { get; set; }
+}
