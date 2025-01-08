2a9d26bf58a522285297b1ef9b7a8fd3deaaff7c
Nikodem Okroj <n.okroj@icloud.com>
2025-01-08 08:10:17 +0100

    refactor: update User model properties to use required and nullable types
    - Modified the User class properties to use 'required' for mandatory fields (Id, Username, FirstName, LastName, Email, PasswordHash, PasswordSalt) and made PhoneNumber nullable.
    - This change enhances the clarity of property requirements and improves data validation within the application.

--- API/Models/User.cs
+++ API/Models/User.cs
@@ -1,12 +1,12 @@
 public class User {
-    public long Id { get; set; }
-    public string Username { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-    public string PhoneNumber { get; set; }
-    public string PasswordHash { get; set; }
-    public string PasswordSalt { get; set; }
+    public required long Id { get; set; }
+    public required string Username { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string Email { get; set; }
+    public string? PhoneNumber { get; set; }
+    public required string PasswordHash { get; set; }
+    public required string PasswordSalt { get; set; }
 
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
