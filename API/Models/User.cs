636a49756a900fe6f8d596adc73ebfcbf9fdfd78
Nikodem Okroj <n.okroj@icloud.com>
2025-01-08 08:12:57 +0100

    refactor: enhance User model with data annotations for validation
    - Added data annotations to the User class properties to enforce validation rules, including [Key], [Required], [EmailAddress], and [Phone].
    - This update improves data integrity and validation within the application, ensuring that mandatory fields are clearly defined and enhancing overall model clarity.

--- API/Models/User.cs
+++ API/Models/User.cs
@@ -1,11 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+
 public class User {
-    public required long Id { get; set; }
+    [Key]
+    public long Id { get; set; }
+    
+    [Required]
     public required string Username { get; set; }
+    
+    [Required]
     public required string FirstName { get; set; }
+    
+    [Required] 
     public required string LastName { get; set; }
+    
+    [Required]
+    [EmailAddress]
     public required string Email { get; set; }
+    
+    [Phone]
     public string? PhoneNumber { get; set; }
+    
+    [Required]
     public required string PasswordHash { get; set; }
+    
+    [Required]
     public required string PasswordSalt { get; set; }
 
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
