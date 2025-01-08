2dbe2c0f007a326468afc23686310b39d8fe8979
Nikodem Okroj <n.okroj@icloud.com>
2025-01-08 16:22:42 +0100

    feat: add initial migration for AuthDbContext with comprehensive database schema
    - Introduced the initial migration for the AuthDbContext, establishing a robust database schema that includes tables for Users, Organizations, Developers, Accounts, Applications, and related entities.
    - Configured relationships, constraints, and foreign keys to ensure data integrity and facilitate complex interactions between entities.
    - Added essential properties, indexes, and default values to enhance query performance and support application functionality.
    - This foundational setup significantly improves the API's capability for user authentication, authorization, and application management.

--- API/Data/Migrations/20250108152204_InitialCreate.Designer.cs
+++ API/Data/Migrations/20250108152204_InitialCreate.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace API.Migrations
+namespace API.Data.Migrations
 {
     [DbContext(typeof(AuthDbContext))]
-    partial class AuthDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250108152204_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
