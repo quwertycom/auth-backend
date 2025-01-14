a59ee8386395e693241bd98693ca0a5501cdd1af
Nikodem Okroj <n.okroj@icloud.com>
2025-01-14 10:54:46 +0100

    feat: add BirthDate and Gender fields to User model
    - Introduced new migration to add BirthDate (DateTime) and Gender (int) columns to the users table.
    - Updated the AuthDbContext model snapshot to reflect these changes.
    - This enhancement allows for better user profile management and data collection.

---
+++ API/Data/Migrations/20250114095248_AddGenderAndBirthDateToUser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGenderAndBirthDateToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BirthDate",
+                table: "users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Gender",
+                table: "users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BirthDate",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "users");
+        }
+    }
+}
