f5c344ffd62e8effa1c43dd79ca0f6da42be06cf
Nikodem Okroj <n.okroj@icloud.com>
2025-01-14 11:51:33 +0100

    feat: enhance user registration logic in AuthService
    - Updated RegisterUserAsync method to include email existence check and password length validation.
    - Improved error handling with specific messages for email already taken, short passwords, and internal server errors.
    - These changes provide clearer feedback during user registration and enhance the overall robustness of the registration process.

--- API/Service/AuthService.cs
+++ API/Service/AuthService.cs
@@ -1,5 +1,7 @@
+using API.Common.Enums;
 using API.Common.Helpers;
 using API.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Service;
 
@@ -19,7 +21,19 @@ public class AuthService : IAuthService
 
     public async Task<(bool isSuccess, string status, string message, long? verificationSessionID)> RegisterUserAsync(string email, string password)
     {
-        await Task.Delay(1000);
-        return (true, "success", "User registered successfully", 1);
+        try {
+            var emailExists = await _dbContext.UserEmails.AnyAsync(ue => ue.Email == email && ue.State == EmailState.Verified);
+            if (emailExists) {
+                return (false, "EMAIL_TAKEN", "Email already exists, please try a different email.", null);
+            }
+            
+            if (password.Length < 8) {
+                return (false, "PASSWORD_TOO_SHORT", "Password must be at least 8 characters long.", null);
+            }
+            
+            return (true, "SUCCESS", "User registered successfully", 1);
+        } catch {
+            return (false, "INTERNAL_SERVER_ERROR", "Internal server error, please try again later or contact support if the issue persists.", null);
+        }
     }
 }
