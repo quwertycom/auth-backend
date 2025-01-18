d9f8e951579e19d12d598854606c5f35abce6823
Nikodem Okroj <n.okroj@icloud.com>
2025-01-18 10:16:39 +0100

    feat: Add configuration classes for database and JWT settings
    - Introduced DatabaseSettings and JwtSettings classes to encapsulate configuration properties for database connection and JWT authentication.
    - These additions enhance the configuration management of the API, providing a structured approach to handle sensitive settings and improving maintainability.

---
+++ API/Configuration/DatabaseSettings.cs
@@ -0,0 +1,7 @@
+public class DatabaseSettings
+{
+    public string Host { get; set; } = null!;
+    public string Database { get; set; } = null!;
+    public string Username { get; set; } = null!;
+    public string Password { get; set; } = null!;
+} 
