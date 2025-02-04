c42f4e67c38e5db27f46b7bb4e81ae27e1890527
Nikodem Okroj <n.okroj@icloud.com>
2025-02-04 09:50:04 +0100

    style: formatted code via dotnet format

--- API/Service/TokenRepository.cs
+++ API/Service/TokenRepository.cs
@@ -3,21 +3,21 @@ using API.Models;
 
 namespace API.Service
 {
-	public interface ITokenRepository
-	{
-		public Task AddToken(Token token);
-	}
-	public class TokenRepository : ITokenRepository
-	{
-		private readonly AuthDbContext _Context;
-		public TokenRepository(AuthDbContext context)
-		{
-			_Context = context;
-		}
-		public async Task AddToken(Token token)
-		{
-			await _Context.Tokens.AddAsync(token);
-			await _Context.SaveChangesAsync();
-		}
-	}
+    public interface ITokenRepository
+    {
+        public Task AddToken(Token token);
+    }
+    public class TokenRepository : ITokenRepository
+    {
+        private readonly AuthDbContext _Context;
+        public TokenRepository(AuthDbContext context)
+        {
+            _Context = context;
+        }
+        public async Task AddToken(Token token)
+        {
+            await _Context.Tokens.AddAsync(token);
+            await _Context.SaveChangesAsync();
+        }
+    }
 }
