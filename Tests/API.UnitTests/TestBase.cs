077a8f955e847e91fa2639e96e644969da05311f
Nikodem Okroj <n.okroj@icloud.com>
2025-03-02 11:59:15 +0100

    refactor: Remove MockRegisterService from TestBase

--- Tests/API.UnitTests/TestBase.cs
+++ Tests/API.UnitTests/TestBase.cs
@@ -18,7 +18,6 @@ public abstract class TestBase
     protected IHasher MockHasher { get; private set; } = null!;
     protected IJwtService MockJwtService { get; private set; } = null!;
     protected IRandomGenerator MockRandomGenerator { get; private set; } = null!;
-    protected IRegisterService MockRegisterService { get; private set; } = null!;
 
     [SetUp]
     public virtual void Setup()
@@ -31,6 +30,5 @@ public abstract class TestBase
         MockHasher = Substitute.For<IHasher>();
         MockJwtService = Substitute.For<IJwtService>();
         MockRandomGenerator = Substitute.For<IRandomGenerator>();
-        MockRegisterService = Substitute.For<IRegisterService>();
     }
 }
